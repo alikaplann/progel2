96b63e1bb53a33d92c96aba1c940702400032daf
Ali Kaplan <ali.kaplan@kobil.com>
2025-05-30 11:54:52 +0300

    unit testing update

--- src/notification/notification.controller.spec.ts
+++ src/notification/notification.controller.spec.ts
@@ -1,9 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificationController } from './notification.controller';
+import { NotificationService } from './notification.service';
+
 
 describe('NotificationController', () => {
   let controller: NotificationController;
-
+  let service: Partial<NotificationService>;
+  const mockNotification = {
+    id: 1,
+    title: 'Notification Title',
+    message: 'Gruba eklendi.',
+    createdAt: new Date('2025-01-01T00:00:00.000Z'),
+    userId: 1,
+    type: ['info', 'error', 'warning']
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificationController],
